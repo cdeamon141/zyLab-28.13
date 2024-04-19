788d3dc18dfef6b7c3328bef7b6ea37acbc4588f
izunga <167342792+izunga@users.noreply.github.com>
2024-04-19 13:38:42 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -4,15 +4,3 @@ using namespace std;
 #include "ItemToPurchase.h"
 
 /* Type your code here */
-string ItemToPurchase::GetName(){
-  return itemName;
-}
-
-int ItemToPurchase::GetPrice(){
-  return itemPrice;
-}
-
-int ItemToPurchase::GetQuantity(){
-  return itemQuantity;
-}
-
