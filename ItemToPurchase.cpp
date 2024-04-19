00b6686ba2958f0cc229ff3ac7006e900c0c0a5d
cdeamon141 <167352063+cdeamon141@users.noreply.github.com>
2024-04-19 13:20:24 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -4,3 +4,7 @@ using namespace std;
 #include "ItemToPurchase.h"
 
 /* Type your code here */
+ItemToPurchase::ItemToPurchase();
+void SetName(string name);
+void SetPrice(int price);
+void SetQuantity(int quantity);
